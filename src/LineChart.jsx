e657ab2bcf81230ca3e21b6a4a0d903054aad502
Shanmukh459 <saikrrish459@gmail.com>
2024-05-24 13:59:10 -0700

    xAxis ticks added

--- src/LineChart.jsx
+++ src/LineChart.jsx
@@ -1,23 +1,37 @@
 import { line, scaleLinear, scaleTime, extent, max } from 'd3'
 import React from 'react'
+import { XAxis } from './XAxis'
 
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 40,
+  left: 20
+}
 export const LineChart = ({data, height, width}) => {
+  const innerWidth = width - margin.left - margin.right
+  const innerHeight = height - margin.top - margin.bottom
 
+  const xValue = d => d.date
   const xScale = scaleTime()
-    .domain(extent(data, d => d.date))
-    .range([0, width])
+    .domain(extent(data, xValue))
+    .range([0, innerWidth])
 
+  const yValue = d => d.totalDeaths
   const yScale = scaleLinear()
-    .domain([0, max(data, d => d.totalDeaths)])
-    .range([height, 0])
+    .domain([0, max(data, yValue)])
+    .range([innerHeight, 0])
 
   const lineGenerator = line()
-  .x(d => xScale(d.date))
-  .y(d => yScale(d.totalDeaths))
+  .x(d => xScale(xValue(d)))
+  .y(d => yScale(yValue(d)))
 
   return (
     <svg height={height} width={width}>
-      <path d={lineGenerator(data)} stroke="black" />
+      <g transform={`translate(${margin.left}, ${margin.top})`}>
+        <XAxis xScale={xScale} innerHeight={innerHeight} />
+        <path d={lineGenerator(data)} />
+      </g>
     </svg>
   )
 }
---
+++ src/XAxis.jsx
@@ -0,0 +1,17 @@
+import { axisBottom, select } from "d3"
+import { useEffect, useRef } from "react"
+
+export const XAxis = ({ xScale, innerHeight }) => {
+  const ref = useRef()
+
+  useEffect(() => {
+    const xAxisG = select(ref.current)
+    const xAxis = axisBottom(xScale)  
+    .tickSize(innerHeight)
+    .tickPadding(5)
+    xAxisG.call(xAxis)
+  }, [])
+  return (
+    <g ref={ref}></g>
+  )
+}
